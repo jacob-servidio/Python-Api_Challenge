abc93e945e7bb5c4effedf43a1b4c033229087e7
jacob-servidio <67562203+jacob-servidio@users.noreply.github.com>
2020-09-08 20:52:20 -0400

    finished code

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "85c2249b6341f45e2db37800cc3a78dd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBpz1vX324bEP1-1tmhp7yGoROttrANUx8"
